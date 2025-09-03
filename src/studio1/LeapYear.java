c59ff1b6508ddc831c8eef8cf31505328981247c
SammyMaayan <m.sammy@wustl.edu>
2025-09-03 14:09:07 -0500

    1

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,12 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner in= new Scanner (System.in);
+        int year =in.nextInt();
+        boolean isLeapYear = (year%4 == 0) && ((year%100 != 0)|| (year%400 == 0));
+        System.out.println(year+" is a leap year: "+isLeapYear);
+    }
+}
